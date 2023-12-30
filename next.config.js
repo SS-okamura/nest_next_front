8d279dc74b971d6c82a59375da39f3c7ca50e098
SS-okamura <ss.okamura0509@gmail.com>
2023-12-31 07:47:44 +0900

    feat: cors対策

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/user/:path",
+        destination: "http://backend:3000/user/:path", // 実際のAPIサーバーのURLに置き換えてください
+      },
+    ];
+  },
+};
