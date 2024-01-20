efb59ebc9b17c284bb9ca9bc554854158439445e
SS-okamura <ss.okamura0509@gmail.com>
2024-01-20 15:48:33 +0900

    feat: Topページの修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { Box, IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const [name, setName] = useState();
+  const router = useRouter();
 
   useEffect(() => {
     getUser();
@@ -28,9 +31,90 @@ export default function Home() {
     name: string;
   };
 
+  const handleTimerClick = () => {
+    router.push("/timer");
+  };
+
+  const handleTaskClick = () => {
+    router.push("/tasks");
+  };
+
+  const handlePomodoroClick = () => {
+    router.push("/pomodoro");
+  };
+
   return (
-    <div>
-      <p>{name}</p>
-    </div>
+    <Box
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh"
+    >
+      <Box sx={{ mr: 2 }}>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={() => handleTaskClick()}
+        >
+          <img
+            src="/Todo.jpeg"
+            alt="My Image"
+            style={{ width: "300px", height: "auto" }}
+          />
+        </IconButton>
+      </Box>
+      <Box sx={{ mr: 5 }}>
+        <img
+          src="/plus.svg"
+          alt="My Image"
+          style={{ width: "100px", height: "auto" }}
+        />
+      </Box>
+      <Box sx={{ mr: 5 }}>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={() => handlePomodoroClick()}
+        >
+          <img
+            src="/tomato.png"
+            alt="My Image"
+            style={{ width: "300px", height: "auto" }}
+          />
+        </IconButton>
+      </Box>
+      <Box sx={{ mr: 5 }}>
+        <img
+          src="/plus.svg"
+          alt="My Image"
+          style={{ width: "100px", height: "auto" }}
+        />
+      </Box>
+      <Box sx={{ mr: 5 }}>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={() => {
+            handleTimerClick();
+          }}
+        >
+          <img
+            src="/timer.png"
+            alt="My Image"
+            style={{ width: "300px", height: "auto" }}
+          />
+        </IconButton>
+      </Box>
+    </Box>
   );
 }
